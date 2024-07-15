5013a64772608324bf30573072c3c28477dde699
vmmarinc <75554704+vmmarinc@users.noreply.github.com>
2024-07-15 12:55:12 -0400

    Delete ForoHub_Challenge directory

--- ForoHub_Challenge/src/main/java/com/alura/forohub/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alura.forohub.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.alura.forohub.model.Curso;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-}

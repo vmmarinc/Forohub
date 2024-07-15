129ec5bdd0338bfd440f84ca71ec996c3d7cad4e
vmmarinc <75554704+vmmarinc@users.noreply.github.com>
2024-07-15 12:53:31 -0400

    Delete ForoHub_Challenge/forohub directory

--- ForoHub_Challenge/forohub/src/main/java/com/alura/forohub/repository/PerfilRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alura.forohub.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.alura.forohub.model.Perfil;
-
-public interface PerfilRepository extends JpaRepository<Perfil, Long> {
-}
--- ForoHub_Challenge/forohub/src/main/java/com/alura/forohub/repository/RespuestaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alura.forohub.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.alura.forohub.model.Respuesta;
-
-public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
-}
